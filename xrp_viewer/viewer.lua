2d231d99aa8156dabc03d5591595dbbf182ce073
Justin Snelgrove <35874535+Itarater@users.noreply.github.com>
2014-05-16 16:38:13 +0000

    xrp_viewer: Temporary Blizzard bug workaround.
    Don't query when showing for character. Can't guarantee it won't
    bollocks things up.

--- xrp_viewer/viewer.lua
+++ xrp_viewer/viewer.lua
@@ -77,7 +77,8 @@ function xrp.viewer:ViewCharacter(name)
 	name = xrp:NameWithRealm(name) -- If there's not a realm, add our realm.
 	current = name
 	self.XC:SetText("")
-	self:Load(xrp.characters[name])
+	-- Can't guarantee inside area of interest. :| Use cache for now.
+	self:Load(xrp.cache[name])
 	-- TODO: Horde/Alliance emblems instead if GF available.
 	SetPortraitToTexture(self.portrait, "Interface\\Icons\\INV_Misc_Book_17")
 	ShowUIPanel(self)
