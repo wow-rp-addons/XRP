03d99aaafcf5b58ed647ccfcb97437dca94fa851
Justin Snelgrove <35874535+Itarater@users.noreply.github.com>
2014-05-29 21:13:44 +0000

    xrp_chatnames: Add more channel types.
    Adds party, raid, instance, and custom/world chats. Also fixes an errant
    pattern match eating half an emote in some cases.

--- xrp_chatnames/chatnames.lua
+++ xrp_chatnames/chatnames.lua
@@ -15,41 +15,76 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-local chatnames = CreateFrame("Frame")
-
 -- And so it begins...
 local old_GetColoredName = _G.GetColoredName
 
 local default_settings = {
 	["CHAT_MSG_SAY"] = true,
 	["CHAT_MSG_YELL"] = true,
-	["CHAT_MSG_EMOTE"] = true,
-	["CHAT_MSG_TEXT_EMOTE"] = true,
-	["CHAT_MSG_GUILD"] = true,
-	["CHAT_MSG_WHISPER"] = true,
-	["CHAT_MSG_WHISPER_INFORM"] = true,
+	["CHAT_MSG_EMOTE"] = true, -- CHAT_MSG_TEXT_EMOTE.
+	["CHAT_MSG_GUILD"] = false, -- CHAT_MSG_OFFICER.
+	["CHAT_MSG_WHISPER"] = false, -- CHAT_MSG_WHISPER_INFORM, CHAT_MSG_AFK, CHAT_MSG_DND
+	["CHAT_MSG_PARTY"] = false, -- CHAT_MSG_PARTY_LEADER
+	["CHAT_MSG_RAID"] = false, -- CHAT_MSG_RAID_LEADER
+	["CHAT_MSG_INSTANCE"] = false, -- CHAT_MSG_INSTANCE_LEADER
+	["CHAT_MSG_CHANNEL"] = false, -- Default for standard/custom channels.
+	["CHAT_MSG_CHANNEL_TRADE"] = false, -- Trade is high volume.
+}
+
+local linked = {
+	["CHAT_MSG_TEXT_EMOTE"] = "CHAT_MSG_EMOTE",
+	["CHAT_MSG_OFFICER"] = "CHAT_MSG_GUILD",
+	["CHAT_MSG_WHISPER_INFORM"] = "CHAT_MSG_WHISPER",
+	["CHAT_MSG_AFK"] = "CHAT_MSG_WHISPER",
+	["CHAT_MSG_DND"] = "CHAT_MSG_WHISPER",
+	["CHAT_MSG_PARTY_LEADER"] = "CHAT_MSG_PARTY",
+	["CHAT_MSG_RAID_LEADER"] = "CHAT_MSG_RAID",
+	["CHAT_MSG_INSTANCE_LEADER"] = "CHAT_MSG_INSTANCE",
+}
+
+local settingsmt = {
+	__index = function(self, chattype)
+		-- This won't get triggered if the user's set a custom setting.
+		if default_settings[chattype] ~= nil then
+			return default_settings[chattype]
+		elseif linked[chattype] then
+			return self[linked[chattype]]
+		elseif chattype:sub(1, 17) == "CHAT_MSG_CHANNEL_" then
+			-- Underscore at end avoids infinite recursion.
+			return self.CHAT_MSG_CHANNEL
+		else
+			return false
+		end
+	end,
 }
 
 -- /cry - I don't want to overwrite your functions, Blizzard, but you don't
 -- leave me any choice.
 local function new_GetColoredName(event, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14)
-	local rpname = false
-	if xrp_settings.chatnames[event] then
-		rpname = true
-	end
+
 	if event == "CHAT_MSG_TEXT_EMOTE" and arg12 then
 		-- No realm for arg2 in TEXT_EMOTEs. For whatever fucking reason.
 		-- Attach it here, falling back to our own realm.
 		arg2 = xrp:NameWithRealm(arg2, (select(7, GetPlayerInfoByGUID(arg12))))
 	end
 
-	local name = rpname and arg12 and xrp.guids[arg12].NA or Ambiguate(arg2, "guild")
 	local chattype = event:sub(10)
 	if chattype:sub(1, 7) == "WHISPER" then
 		chattype = "WHISPER"
 	elseif chattype:sub(1, 7) == "CHANNEL" then
 		chattype = "CHANNEL"..arg8
 	end
+
+	-- RP name in channels is from case-insensitive NAME, not the number.
+	if event == "CHAT_MSG_CHANNEL" and type(arg9) == "string" then
+		event = event.."_"..arg9:upper()
+	end
+
+	local rpname = false
+	if xrp_settings.chatnames[event] then
+		rpname = true
+	end
+	local name = rpname and arg12 and xrp.guids[arg12].NA or Ambiguate(arg2, "guild")
 
 	local info = ChatTypeInfo[chattype]
 	if info and info.colorNameByClass and arg12 then
@@ -62,36 +97,47 @@ local function new_GetColoredName(event, arg1, arg2, arg3, arg4, arg5, arg6, arg
 	return name
 end
 
-local stripname = "^"..CHAT_EMOTE_GET:format(FULL_PLAYER_NAME:format(".-", ".-")).."(.*)$"
+local stripname = "^"..CHAT_EMOTE_GET:format(FULL_PLAYER_NAME:format("%s", ".-")).."(.*)"
 local function emotename(self, event, message, sender, ...)
+	-- Availability of GUID for restoring the realm. Bail out if we won't be
+	-- able to recover the realm name later.
 	if not (select(10, ...)) then
 		return false
 	end
+
 	-- The other half of attaching the realm name in GetColoredName is to,
 	-- uh, remove it here first. Why? Fuck knows, it's Blizzard and we get
 	-- things like Player-RealmName-RealmName if we don't drop it here from
 	-- the message. ...Which is where the name is, because fuck knows.
-	local nameless = message:match(stripname)
+	local nameless = message:match(stripname:format(sender))
 	if nameless then
 		message = CHAT_EMOTE_GET:format(sender)..nameless
 	end
 	return false, message, sender, ...
 end
 
+local chatnames = CreateFrame("Frame")
 local function chatnames_OnEvent(self, event, addon)
 	if event == "ADDON_LOADED" and addon == "xrp_chatnames" then
 		if type(xrp_settings.chatnames) ~= "table" then
 			xrp_settings.chatnames = {}
+		else
+			-- Pre-5.4.8.0_beta2 conversion.
+			for _, chattype in ipairs({ "CHAT_MSG_TEXT_EMOTE", "CHAT_MSG_WHISPER_INFORM" }) do
+				if xrp_settings.chatnames[chattype] ~= nil then
+					xrp_settings.chatnames[chattype] = nil
+				end
+			end
 		end
-		setmetatable(xrp_settings.chatnames, { __index = default_settings })
+		setmetatable(xrp_settings.chatnames, settingsmt)
 		ChatFrame_AddMessageEventFilter("CHAT_MSG_TEXT_EMOTE", emotename)
 		self:UnregisterEvent("ADDON_LOADED")
 		self:RegisterEvent("PLAYER_LOGIN")
 	elseif event == "PLAYER_LOGIN" then
 		-- I hate this. Hate hate hate hate hate.
 		_G.GetColoredName = new_GetColoredName
+		self:UnregisterEvent("PLAYER_LOGIN")
 	end
 end
-
 chatnames:SetScript("OnEvent", chatnames_OnEvent)
 chatnames:RegisterEvent("ADDON_LOADED")
