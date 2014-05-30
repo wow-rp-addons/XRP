affe0fafbb92e20ff5cac6bbc07654e0bc136c27
Justin Snelgrove <35874535+Itarater@users.noreply.github.com>
2014-05-30 03:44:24 +0000

    xrp_options: Expand chatnames options.
    This expands the chatnames options to include all recently added
    channels, including custom channels.

--- xrp_options/chatnames.lua
+++ xrp_options/chatnames.lua
@@ -15,15 +15,44 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-local chatnames_types = { "CHAT_MSG_SAY", "CHAT_MSG_YELL", "CHAT_MSG_EMOTE", "CHAT_MSG_GUILD", "CHAT_MSG_WHISPER" }
+local chatnames_types = { "CHAT_MSG_SAY", "CHAT_MSG_YELL", "CHAT_MSG_EMOTE", "CHAT_MSG_GUILD", "CHAT_MSG_WHISPER", "CHAT_MSG_PARTY", "CHAT_MSG_RAID", "CHAT_MSG_INSTANCE", "CHAT_MSG_CHANNEL" }
+
+local channel_menu = {}
+local channel_list = {}
+
+local function chatnames_Channels(...)
+	local p = 1
+	local list = {}
+	while select(p, ...) do
+		list[select(p, ...)] = select(p + 1, ...)
+		p = p + 2
+	end
+	return list
+end
+
+local function chatnames_UpdateChannelSetting(self, arg1, arg2, checked)
+	channel_list[arg1].checked = not checked
+end
+
+local function chatnames_UpdateChannels()
+	local chanlist = chatnames_Channels(GetChannelList())
+	wipe(channel_menu)
+	wipe(channel_list)
+	for _, name in ipairs(chanlist) do
+		channel = "CHAT_MSG_CHANNEL_"..name:upper()
+		channel_menu[#channel_menu + 1] = { text = name, arg1 = channel, isNotRadio = true, checked = xrp_settings.chatnames[channel], func = chatnames_UpdateChannelSetting }
+		channel_list[channel] = channel_menu[#channel_menu]
+	end
+	channel_menu[#channel_menu + 1] = { text = CLOSE, notCheckable = true }
+end
 
 local function chatnames_Okay()
-	for _, chat in ipairs(chatnames_types) do
-		xrp_settings.chatnames[chat] = xrp.options.chatnames[chat]:GetChecked() and true or false
-		if chat == "CHAT_MSG_WHISPER" then
-			xrp_settings.chatnames["CHAT_MSG_WHISPER_INFORM"] = xrp.options.chatnames[chat]:GetChecked() and true or false
-		elseif chat == "CHAT_MSG_EMOTE" then
-			xrp_settings.chatnames["CHAT_MSG_TEXT_EMOTE"] = xrp.options.chatnames[chat]:GetChecked() and true or false
+	for _, chattype in ipairs(chatnames_types) do
+		xrp_settings.chatnames[chattype] = xrp.options.chatnames[chattype]:GetChecked() and true or false
+	end
+	for chattype, menu in pairs(channel_list) do
+		if menu.text ~= CLOSE then
+			xrp_settings.chatnames[chattype] = menu.checked
 		end
 	end
 end
@@ -32,27 +61,31 @@ local function chatnames_Refresh()
 	for _, chat in ipairs(chatnames_types) do
 		xrp.options.chatnames[chat]:SetChecked(xrp_settings.chatnames[chat])
 	end
+	chatnames_UpdateChannels()
 end
 
 local function chatnames_Default()
-	for chat, setting in pairs(xrp_settings.chatnames) do
+	for chat, _ in pairs(xrp_settings.chatnames) do
 		xrp_settings.chatnames[chat] = nil
 	end
-
 	chatnames_Refresh()
+end
+
+local function chatnames_ChannelButton_OnClick(self, button, down)
+	EasyMenu(channel_menu, self:GetParent().CHANNEL_MENU, self, 3, 10, "MENU", nil)
 end
 
 local function chatnames_OnEvent(self, event, addon)
 	if event == "ADDON_LOADED" and addon == "xrp_options" then
 
-		local name, title, notes, enabled, loadable, reason = GetAddOnInfo("xrp_chatnames")
 		self.name = "Chat Names"
 		self.refresh = chatnames_Refresh
 		self.okay = chatnames_Okay
 		self.default = chatnames_Default
 		self.parent = XRP
+		self.CHANNEL_BUTTON:SetScript("OnClick", chatnames_ChannelButton_OnClick)
 
-		if enabled then
+		if (select(4, GetAddOnInfo("xrp_chatnames"))) then
 			InterfaceOptions_AddCategory(self)
 			chatnames_Refresh()
 		end
