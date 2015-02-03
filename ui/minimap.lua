64054b165611bdd38e44f290115199c26ae297de
Justin Snelgrove <35874535+Itarater@users.noreply.github.com>
2015-02-03 07:31:32 +0000

    More copyright year cleanups.

--- ui/minimap.lua
+++ ui/minimap.lua
@@ -1,5 +1,6 @@
 --[[
 	© Justin Snelgrove
+	(C) 2008-2011 Rabbit <rabbit.magtheridon@gmail.com>
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
