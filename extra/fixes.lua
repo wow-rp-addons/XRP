c3f2f3bda623b95a770e2fc5668ab3ec30e11d27
Justin Snelgrove <35874535+Itarater@users.noreply.github.com>
2015-09-22 03:38:53 +0000

    Fix UIDropDownMenu/Interface Options tainting
    This adds a new, embeddable piece of code to fix the UIDropDownMenu
    tainting issues from any/all addons, as far as I've been able to
    determine, as well as the issue with tainting the Interface Options
    panel. Making normal use of the dropdown menus or the addon section of
    Interface Options should, when running this fix, never taint the UI.
    
    The primary method of testing the tainting is through the infamous
    issues with the built-in compact unit frames profiles interface options
    panel, but the fixes should be generalized enough to stop the tainting
    from ever occurring elsewhere, too.
    
    In addition, the old warning code has been stripped out, as it no longer
    applies with the new taint-preventing, rather than just taint-warning.

--- extra/fixes.lua
+++ extra/fixes.lua
@@ -17,39 +17,6 @@
 
 local addonName, _xrp = ...
 
--- Making use of the raid profile dropdowns to change settings taints the
--- entire default raid frames. This warns if such changes have been made.
-local doWarning
-hooksecurefunc("CompactUnitFrameProfilesDropdownButton_OnClick", function(self, dropDown)
-	doWarning = true
-end)
-
-InterfaceOptionsFrame:HookScript("OnHide", function(self)
-	if not doWarning then return end
-	doWarning = nil
-	StaticPopup_Show("XRP_RELOAD", _xrp.L.CUF_WARNING)
-end)
-
--- This is kinda terrifying, but it fixes some major UI tainting when the user
--- presses "Cancel" in the Interface Options (out of combat). The drawback is
--- that any changes made to the default compact raid frames aren't actually
--- cancelled (they're not saved, but they're still active). Still, this is
--- better than having the Cancel button completely taint the raid frames, and
--- then have that taint spread further.
-function CompactUnitFrameProfiles_CancelChanges(self)
-	InterfaceOptionsPanel_Cancel(self)
-
-	-- The following is disabled to make it more obvious that changes aren't
-	-- really cancelled.
-	--RestoreRaidProfileFromCopy()
-
-	CompactUnitFrameProfiles_UpdateCurrentPanel()
-
-	-- The following is disabled because it's the actual function that taints
-	-- everything.
-	--CompactUnitFrameProfiles_ApplyCurrentSettings()
-end
-
 -- ElvUI (and some others) disable the entire HelpPlate system rather than
 -- specifically managing it on the frames they modify or handling the automated
 -- tutorials in a more granular way.
