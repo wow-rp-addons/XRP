782d7fe089ad95163ad4962ff71be5aea627ffe1
Solanya <solanya@totalrp3.info>
2022-06-05 15:41:07 +0200

    Right-click menu rework

--- Interface/Integration/Menus.lua
+++ Interface/Integration/Menus.lua
@@ -23,159 +23,79 @@ local L = AddOn.GetText
 -- This adds "Roleplay Profile" menu entries to several menus for a more
 -- convenient way to access profiles (including chat names, guild lists, and
 -- chat rosters).
---
--- Note: Cannot be added to menus which call protected functions without
--- causing taint problems. This includes all unit menus with a "SET_FOCUS"
--- button. The menus can be found in Blizzard's UnitPopups.lua.
 
-local standard, units
-
-local function UnitPopup_OnClick_Hook(self)
-	if not standard and not units then return end
-	if self.value == "XRP_VIEW_CHARACTER" then
-		XRPViewer:View(AddOn_Chomp.NameMergedRealm(UIDROPDOWNMENU_INIT_MENU.name, UIDROPDOWNMENU_INIT_MENU.server))
-	elseif self.value == "XRP_VIEW_UNIT" then
+local function OpenPlayerProfile()
+	if UnitExists(UIDROPDOWNMENU_INIT_MENU.unit) then
 		XRPViewer:View(UIDROPDOWNMENU_INIT_MENU.unit)
-	elseif self.value == "XRP_VIEW_BN" then
-		local gameAccountInfo = C_BattleNet.GetAccountInfoByID(UIDROPDOWNMENU_INIT_MENU.bnetIDAccount).gameAccountInfo;
-		local characterName = gameAccountInfo.characterName or "";
-		local client = gameAccountInfo.clientProgram;
-		local realmName = gameAccountInfo.realmName or "";
-		if client == BNET_CLIENT_WOW and realmName ~= "" then
-			XRPViewer:View(AddOn_Chomp.NameMergedRealm(characterName, realmName))
-		end
+	else
+		XRPViewer:View(AddOn_Chomp.NameMergedRealm(UIDROPDOWNMENU_INIT_MENU.name, UIDROPDOWNMENU_INIT_MENU.server))
 	end
 end
 
-local function UnitPopup_HideButtons_Hook()
-	if not standard or UIDROPDOWNMENU_INIT_MENU.which ~= "BN_FRIEND" or UIDROPDOWNMENU_MENU_VALUE and UIDROPDOWNMENU_MENU_VALUE ~= "BN_FRIEND" then return end
-	for i, button in ipairs(UnitPopupMenus["BN_FRIEND"]) do
-		if button == "XRP_VIEW_BN" then
-			if not UIDROPDOWNMENU_INIT_MENU.bnetIDAccount then
-				UnitPopupShown[UIDROPDOWNMENU_MENU_LEVEL][i] = 0
-			else
-				local gameAccountInfo = C_BattleNet.GetAccountInfoByID(UIDROPDOWNMENU_INIT_MENU.bnetIDAccount).gameAccountInfo;
-				local client = gameAccountInfo.clientProgram;
-				local realmName = gameAccountInfo.realmName or "";
-				if client ~= BNET_CLIENT_WOW or realmName == "" then
-					UnitPopupShown[UIDROPDOWNMENU_MENU_LEVEL][i] = 0
-				else
-					UnitPopupShown[UIDROPDOWNMENU_MENU_LEVEL][i] = 1
-				end
-			end
-			break
-		end
+local function OpenBNetProfile()
+	local gameAccountInfo = C_BattleNet.GetAccountInfoByID(UIDROPDOWNMENU_INIT_MENU.bnetIDAccount).gameAccountInfo;
+	local characterName = gameAccountInfo.characterName or "";
+	local client = gameAccountInfo.clientProgram;
+	local realmName = gameAccountInfo.realmName or "";
+	if client == BNET_CLIENT_WOW and realmName ~= "" then
+		XRPViewer:View(AddOn_Chomp.NameMergedRealm(characterName, realmName))
 	end
 end
 
-local Buttons_Profile = { text = L.ROLEPLAY_PROFILE }
+local buttons = {
+	player = {text = L.ROLEPLAY_PROFILE, func = OpenPlayerProfile, notCheckable = true},
+	bnet = {text = L.ROLEPLAY_PROFILE, func = OpenBNetProfile, notCheckable = true},
+}
 
-local isHooked
-AddOn.SettingsToggles.menusChat = function(setting)
-	if setting then
-		if not isHooked then
-			hooksecurefunc("UnitPopup_OnClick", UnitPopup_OnClick_Hook)
-			isHooked = true
-		end
-		if standard == nil then
-			hooksecurefunc("UnitPopup_HideButtons", UnitPopup_HideButtons_Hook)
-			for i, button in ipairs(UnitPopupMenus["FRIEND"]) do
-				if button == "PVP_REPORT_AFK" then
-					table.remove(UnitPopupMenus["FRIEND"], i)
-				end
-			end
-		end
-		if not standard then
-			UnitPopupButtons["XRP_VIEW_CHARACTER"] = Buttons_Profile
-			UnitPopupButtons["XRP_VIEW_BN"] = Buttons_Profile
-			table.insert(UnitPopupMenus["FRIEND"], 1, "XRP_VIEW_CHARACTER")
-			table.insert(UnitPopupMenus["GUILD"], 1, "XRP_VIEW_CHARACTER")
-			table.insert(UnitPopupMenus["CHAT_ROSTER"], 1, "XRP_VIEW_CHARACTER")
-			table.insert(UnitPopupMenus["BN_FRIEND"], 1, "XRP_VIEW_BN")
-		end
-		standard = true
-	elseif standard ~= nil then
-		UnitPopupButtons["XRP_VIEW_CHARACTER"] = nil
-		UnitPopupButtons["XRP_VIEW_BN"] = nil
-		for i, button in ipairs(UnitPopupMenus["FRIEND"]) do
-			if button == "XRP_VIEW_CHARACTER" then
-				table.remove(UnitPopupMenus["FRIEND"], i)
-				break
-			end
-		end
-		for i, button in ipairs(UnitPopupMenus["GUILD"]) do
-			if button == "XRP_VIEW_CHARACTER" then
-				table.remove(UnitPopupMenus["GUILD"], i)
-				break
-			end
-		end
-		for i, button in ipairs(UnitPopupMenus["CHAT_ROSTER"]) do
-			if button == "XRP_VIEW_CHARACTER" then
-				table.remove(UnitPopupMenus["CHAT_ROSTER"], i)
-				break
-			end
-		end
-		for i, button in ipairs(UnitPopupMenus["BN_FRIEND"]) do
-			if button == "XRP_VIEW_BN" then
-				table.remove(UnitPopupMenus["BN_FRIEND"], i)
-				break
-			end
-		end
-		standard = false
+local allowedUnits = {
+	CHAT_ROSTER = "player",
+	FRIEND = "player",
+	FRIEND_OFFLINE = "player",
+	BN_FRIEND = "bnet",
+	GUILD = "player",
+	GUILD_OFFLINE = "player",
+	PARTY = "player",
+	PLAYER = "player",
+	RAID = "player",
+	RAID_PLAYER = "player",
+}
+
+local function UnitPopup_OnShowMenu_Hook(dropdownMenu, menuType)
+	if not dropdownMenu or dropdownMenu:IsForbidden() then
+		return  -- Invalid or forbidden menu.
+	elseif UIDROPDOWNMENU_MENU_LEVEL ~= 1 then
+		return  -- We don't support submenus.
 	end
+
+	if not allowedUnits[menuType] then
+		return  -- No buttons to be shown.
+	end
+
+	if menuType == "BN_FRIEND" then
+		if not UIDROPDOWNMENU_INIT_MENU.bnetIDAccount then
+			return
+		else
+			local gameAccountInfo = C_BattleNet.GetAccountInfoByID(UIDROPDOWNMENU_INIT_MENU.bnetIDAccount).gameAccountInfo
+			local client = gameAccountInfo.clientProgram
+			local realmName = gameAccountInfo.realmName or ""
+			if client ~= BNET_CLIENT_WOW or realmName == "" then
+				return
+			end
+		end
+	end
+
+	if UnitExists(UIDROPDOWNMENU_INIT_MENU.unit) then
+		if not xrpAccountSaved.settings.menusUnits then
+			return
+		end
+	else
+		if not xrpAccountSaved.settings.menusChat then
+			return
+		end
+	end
+
+	UIDropDownMenu_AddSeparator()
+	UIDropDownMenu_AddButton(buttons[allowedUnits[menuType]], UIDROPDOWNMENU_MENU_LEVEL)
 end
 
-AddOn.SettingsToggles.menusUnits = function(setting)
-	if setting then
-		if not isHooked then
-			hooksecurefunc("UnitPopup_OnClick", UnitPopup_OnClick_Hook)
-			isHooked = true
-		end
-		if units == nil then
-			for i, button in ipairs(UnitPopupMenus["PLAYER"]) do
-				if button == "SET_FOCUS" then
-					table.remove(UnitPopupMenus["PLAYER"], i)
-					break
-				end
-			end
-			for i, button in ipairs(UnitPopupMenus["PARTY"]) do
-				if button == "SET_FOCUS" or button == "PVP_REPORT_AFK" then
-					table.remove(UnitPopupMenus["PARTY"], i)
-				end
-			end
-			for i, button in ipairs(UnitPopupMenus["RAID_PLAYER"]) do
-				if button == "SET_FOCUS" or button == "PVP_REPORT_AFK" then
-					table.remove(UnitPopupMenus["RAID_PLAYER"], i)
-				end
-			end
-		end
-		if not units then
-			UnitPopupButtons["XRP_VIEW_UNIT"] = Buttons_Profile
-			table.insert(UnitPopupMenus["PLAYER"], 1, "XRP_VIEW_UNIT")
-			table.insert(UnitPopupMenus["PARTY"], 1, "XRP_VIEW_UNIT")
-			table.insert(UnitPopupMenus["RAID_PLAYER"], 1, "XRP_VIEW_UNIT")
-		end
-		units = true
-	elseif units ~= nil then
-		UnitPopupButtons["XRP_VIEW_UNIT"] = nil
-		for i, button in ipairs(UnitPopupMenus["PLAYER"]) do
-			if button == "XRP_VIEW_UNIT" then
-				table.remove(UnitPopupMenus["PLAYER"], i)
-				break
-			end
-		end
-		for i, button in ipairs(UnitPopupMenus["PARTY"]) do
-			if button == "XRP_VIEW_UNIT" then
-				table.remove(UnitPopupMenus["PARTY"], i)
-				break
-			end
-		end
-		for i, button in ipairs(UnitPopupMenus["RAID_PLAYER"]) do
-			if button == "XRP_VIEW_UNIT" then
-				table.remove(UnitPopupMenus["RAID_PLAYER"], i)
-				break
-			end
-		end
-		units = false
-	end
-end
+hooksecurefunc("UnitPopup_ShowMenu", UnitPopup_OnShowMenu_Hook)
